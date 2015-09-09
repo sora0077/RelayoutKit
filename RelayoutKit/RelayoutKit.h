82c19bba4621f75fd54b3f4770ed0e4a4797dcb8
驕比ｹ滓棊 <t_hayashi@hayashitatsuya-no-mac-pro.local>
2015-09-09 21:37:12 +0900

    Initial Commit

---
+++ RelayoutKit/RelayoutKit.h
@@ -0,0 +1,19 @@
+//
+//  RelayoutKit.h
+//  RelayoutKit
+//
+//  Created by 林達也 on 2015/09/09.
+//  Copyright © 2015年 jp.sora0077. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RelayoutKit.
+FOUNDATION_EXPORT double RelayoutKitVersionNumber;
+
+//! Project version string for RelayoutKit.
+FOUNDATION_EXPORT const unsigned char RelayoutKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RelayoutKit/PublicHeader.h>
+
+
